19df11af1d2793bd4746a1808e02ecaca5bec41d
balik18 <55997609+YigitBalik@users.noreply.github.com>
2022-01-31 14:23:47 +0300

    Bug fix

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ app.add_url_rule('/favoriteEvents/delete/<int:favoriteListID>/<int:eventid>', en
 app.add_url_rule('/favoriteEvents/deleteInList/<int:eventid>', endpoint="deleteEventFromFavoriteEventsInList", view_func=deleteEventFromFavoriteEventsInFavList, methods=['POST'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)))
