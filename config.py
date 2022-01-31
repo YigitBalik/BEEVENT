6b6e40244ad6486248f0f3bb3df52ae109ed0522
balik18 <55997609+YigitBalik@users.noreply.github.com>
2022-01-31 14:04:25 +0300

    environ settings

--- config.py
+++ config.py
@@ -1,15 +1,3 @@
-import CREDENTIALS
 import os
-
-connectionDSN = """"""
-
-HOME_PATH = os.path.expanduser("~").lower()
-if 'mybal' in HOME_PATH:
-    connectionDSN = CREDENTIALS.DSN_MYB
-elif 'mihri' in HOME_PATH:
-    connectionDSN = CREDENTIALS.DSN_MIHRI
-elif 'application' in HOME_PATH:
-    connectionDSN = CREDENTIALS.DSN_DEPLOYMENT
-else:
-    connectionDSN = CREDENTIALS.DSN_OTHER
-APIKEY = CREDENTIALS.APIKEY
+connectionDSN = os.environ.get('DATABASE_URL')
+APIKEY = os.environ.get('APIKEY')
